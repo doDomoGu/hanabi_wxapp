75db8d53b1fdebef3e9722e1c2dc9bd007b82182
doDomoGu <kaka_71936410@qq.com>
2018-02-11 22:33:14 +0800

    request 本地测试

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    wx.request({
+      url: 'http://localhost:8889/v1/user',
+    })
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
