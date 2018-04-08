c3442f63220624ece07f378a89a819105ab98e0f
doDomoGu <kaka_71936410@qq.com>
2018-04-08 11:55:39 +0800

    wxauth 调试

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
 App({
   globalData : {
-    apiBaseUrl: "http://192.168.31.176:8889/v1",
+    apiBaseUrl: "http://192.168.0.100:8889/v1",
+
   },
   onLaunch: function () {
-    var that = this
+    // let that = this
+    // wx.login({
+    //   success: res => {
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //     wx.request({
+    //       url: this.globalData.apiBaseUrl + '/wxauth/code2session?jscode=' + res.code,
+    //       success: function (res) {
+    //         if (res.data && res.data.success) {
+    //           //获得 请求接口需要用到的token
+    //           wx.setStorageSync('token', res.data.token)
+    //
+    //           that.globalData.userInfo = {id:res.data.userid}
+    //
+    //
+    //           console.log(that.globalData)
+    //
+    //
+    //           wx.reLaunch({
+    //             url: 'pages/game/index'
+    //           })
+    //         }
+    //       }
+    //     })
+    //   },
+    //   fail : res => {
+    //     console.log(res)
+    //   }
+    // })
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.push({id:logs.length+1 , logTime:Date.now()})
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,15 +1,19 @@
 //index.js
 //获取应用实例
-const app = getApp()
+//const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World' ,
+    motto: 'Hello World',
+    userInfoShow: false,
+    userMottoShow: false,
     userInfo: {},
     userId: null,
     hasUserInfo: false,
-    roomList: [],
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    isInRoom: false,
+    isInGame: false,
+    //canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    apiBaseUrl: "http://192.168.0.100:8889/v1"
   },
   //事件处理函数
   bindViewTap: function() {
@@ -19,43 +23,131 @@ Page({
   },
 
   onLoad: function () {
+    // const ctx = wx.createCanvasContext('myCanvas')
+    //
+    // ctx.draw()
+
+
     const that = this
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        wx.request({
-          url: app.globalData.apiBaseUrl + '/wxauth/code2session?jscode=' + res.code,
-          success: function (res) {
-            if (res.data && res.data.success) {
-              //获得 请求接口需要用到的token
-              wx.setStorageSync('token', res.data.token)
-              //获得 用户ID
-              that.setData({
-                userId: res.data.userid,
-                motto: that.data.motto + '(' + res.data.userid + ')'
-              })
 
-              that.getRoom()
-            }
+    let token = wx.getStorageSync('token') || ''
+    console.log(token)
+    if (token != '') {
+      wx.request({
+        url: this.data.apiBaseUrl + '/wxauth?token=' + token,
+        success: function (res) {
+          if (res.data && res.data.success) {
+            console.log(41)
+            //获得 请求接口需要用到的token
+            // wx.setStorageSync('token', res.data.token)
+            // //获得 用户ID
+            // that.setData({
+            //   userId: res.data.userid,
+            //   motto: that.data.motto + '(' + res.data.userid + ')'
+            // })
+
+            //that.getRoom()
+          } else {
+            wx.removeStorageSync('token')
           }
-        })
-      },
-      fail : res => {
-        console.log(res)
-      }
-    })
-    wx.getUserInfo({
-      success: res => {
-        // 可以将 res 发送给后台解码出 unionId
-        this.setData({
-          userInfo : res.userInfo,
-          hasUserInfo : true
-        })
-      }
-    })
+        }
+      })
+    }else{
+      // 登录
+      wx.login({
+        success: res => {
+          // 发送 res.code 到后台换取 openId, sessionKey, unionId
+          console.log(61)
+          console.log(res)
+          wx.request({
+            url: this.data.apiBaseUrl + '/wxauth/code2session?jscode=' + res.code,
+            success: function (res) {
+              if (res.data && res.data.success) {
+                //获得 请求接口需要用到的token
+                wx.setStorageSync('token', res.data.token)
+                //获得 用户ID
+                that.setData({
+                  userId: res.data.userid,
+                  motto: that.data.motto + '(' + res.data.userid + ')'
+                })
+
+                //that.getRoom()
+              }
+            }
+          })
+        },
+        fail : res => {
+          console.log('login fail')
+          console.log(res)
+        }
+      })
+    }
   },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //
+  // onReady:function(){
+  //   // 页面渲染完成
+  //   //第一步创建个上下文容器
+  //   let ctx = wx.createCanvasContext('main', this)
+  //
+  //   //第二步绘制这里我们绘制个矩形
+  //   ctx.strokeRect(50, 50, 100, 100)
+  //   ctx.draw()
+  //
+  //
+  //   console.log(ctx)
+  //
+  // },
+
   getRoom: function() {
     console.log(this)
     wx.request({
