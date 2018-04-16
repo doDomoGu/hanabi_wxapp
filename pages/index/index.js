8cfc9713a16a917296bbd5dcaa7a64183b5bc0e8
doDomoGu <kaka_71936410@qq.com>
2018-04-16 17:52:29 +0800

    onload 获取用户是否在房间/是否在游戏中状态

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,50 +16,34 @@ Page({
     height: 0,
     ratio: 1,
     //canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    apiBaseUrl: "http://192.168.31.176:8889/v1"
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  bindButtonTap: function(r,a,b) {
-    const page = r.currentTarget.dataset.page
-    var isInRoom
-    var isInGame
-    if (page == 1){
-      isInRoom = false
-      isInGame = false
-    } else if(page == 2){
-      isInRoom = true
-      isInGame = false
-    } else if(page == 3){
-      isInRoom = true
-      isInGame = true
+    apiBaseUrl: "http://192.168.0.110:8889/v1",
+    roomList:[],
+    canvasParam: {
+      topLeftPad: 0,
+      topWidth: 0,
     }
-
-    this.setData({
-      isInRoom: isInRoom,
-      isInGame: isInGame
-    })
   },
-
   onLoad: function () {
     const that = this
-
-    this.canvasInit()
-
-
-
     // 1.登录验证 （获得用户ID）
     console.log('step 1')
-    that.login().then(function(res){
-      console.log(res)
+    that.login().then(function(){
+      // 2.根据用户ID 获取当前状态（在房间中/在游戏中）
       console.log('step 2')
+      return that.getStatus()
+    }).then(function(){
+      console.log(that.data.isInRoom)
+      console.log(that.data.isInGame)
+      return that.canvasInit()
+    }).then(function(){
+      return that.drawRoomList()
     })
 
-    // 2.根据用户ID 获取当前状态（在房间中/在游戏中）
+
+
+
+    //获取房间列表
+    //this.getRoomList()
 
 
 
@@ -69,42 +53,161 @@ Page({
 
     })*/
   },
+
+
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindButtonTap: function(r) {
+    const page = parseInt(r.currentTarget.dataset.page)
+
+    let isInRoom
+    let isInGame
+    if (page === 1){
+      isInRoom = false
+      isInGame = false
+    } else if(page === 2){
+      isInRoom = true
+      isInGame = false
+    } else if(page === 3){
+      isInRoom = true
+      isInGame = true
+    }
+
+    this.setData({
+      isInRoom: isInRoom,
+      isInGame: isInGame
+    })
+  },
+  //
+  getStatus: function () {
+    const that = this
+
+    let getRoomId = function() {
+      return new Promise(function (resolve, reject) {
+        let token = wx.getStorageSync('token') || ''
+        wx.request({
+          method: "POST",
+          url: that.data.apiBaseUrl + '/my-room/get-info?token=' + token,
+          data: {
+            mode: 'simple',
+            force: true
+          },
+          success: res => {
+            if (res.data && res.data.success) {
+              resolve(res.data.data.roomId)
+            } else {
+              resolve(res.data.data.roomId)
+            }
+          },
+          fail: error => {
+            reject(error)
+          }
+        })
+      })
+    }
+
+    let getIsInGame = function() {
+      return new Promise(function (resolve, reject) {
+        let token = wx.getStorageSync('token') || ''
+        wx.request({
+          method: "POST",
+          url: that.data.apiBaseUrl + '/my-game/get-info?token=' + token,
+          data: {
+            mode: 'simple',
+            force: true
+          },
+          success: res => {
+            if (res.data && res.data.success) {
+              resolve(true)
+            } else {
+              resolve(false)
+            }
+          },
+          fail: error => {
+            reject(error)
+          }
+        })
+      })
+    }
+
+    return new Promise(function (resolve, reject) {
+      getRoomId().then(function(room_id){
+        if(room_id>0){
+          // that.setData({
+          //   isInRoom: true
+          // })
+          console.log(142)
+          return getIsInGame().then(function(isInGame){
+            that.setData({
+              isInRoom: true,
+              isInGame: isInGame
+            })
+            resolve()
+          })
+        }else{
+          that.setData({
+            isInRoom: false,
+            isInGame: false
+          })
+          resolve()
+        }
+      })
+    })
+
+
+
+  },
   // 画布初始化 ：获得并设置高度/宽度/像素比
   canvasInit: function () {
     const that = this
     return new Promise(function (resolve, reject) {
-      wx.getSystemInfo({
-        success: function (res) {
-          that.setData({
-            width : res.windowWidth,
-            height: res.windowHeight,
-            ratio : res.pixelRatio
-          })
-          // that.radius = 105 / 602 * that.height
-          // console.log(res.model)
-          // console.log(res.pixelRatio)
-          // console.log(res.windowWidth)
-          // console.log(res.windowHeight)
-          // console.log(res.language)
-          // console.log(res.version)
-          // console.log(res.platform)
+      setTimeout(function(){
+        wx.getSystemInfo({
+          success: function (res) {
+            that.setData({
+              width : res.windowWidth,
+              height: res.windowHeight,
+              ratio : res.pixelRatio
+            })
 
-          // const ctxRoomList = wx.createCanvasContext('roomListCanvas')
-          // const ctxMyRoom = wx.createCanvasContext('myRoomCanvas')
-          // const ctxMyGame = wx.createCanvasContext('myGameCanvas')
+            let p = that.data.canvasParam
+            const ratio = that.data.ratio
+            const width = that.data.width
+            p.topLeftPad = 10 * ratio // 左侧pad
+            p.topWidth = width - p.topLeftPad * 2 // 去除左右pad后的宽度
+            that.setData({
+              canvasPararm : p
+            })
+            // that.radius = 105 / 602 * that.height
+            // console.log(res.model)
+            // console.log(res.pixelRatio)
+            // console.log(res.windowWidth)
+            // console.log(res.windowHeight)
+            // console.log(res.language)
+            // console.log(res.version)
+            // console.log(res.platform)
+            //console.log(that.data.canvasParam)
 
 
 
+            // context.setFillStyle("#ff00ff");
+            // context.setStrokeStyle("#00ffff");
+            //
+            // context.rect(50,50,100,100)
+            // context.fill()
+            // context.stroke()
 
+            //ctx.stroke();
 
-          /*that.canvasInit().then(function() {
+            resolve()
+          }
+        })
+      },2000)
 
-          })*/
-
-
-          resolve()
-        }
-      })
     })
   },
 
@@ -168,10 +271,10 @@ Page({
                   //获得 请求接口需要用到的token
                   wx.setStorageSync('token', res.data.token)
                   //获得 用户ID
-                  that.setData({
-                    userId: res.data.user_id,
-                    motto: that.data.motto + '(' + res.data.user_id + ')'
-                  })
+                  // that.setData({
+                  //   userId: res.data.user_id,
+                  //   motto: that.data.motto + '(' + res.data.user_id + ')'
+                  // })
 
                   //that.getRoom()
 
@@ -202,10 +305,14 @@ Page({
           return codeAuth()
         }
       }).then(function(res){
+        that.setData({
+          userId: res.user_id,
+        })
+
         console.log(res)
         console.log('打印 userid :' +   res.user_id)
         console.log('结束')
-        resolve()
+        resolve(res.user_id)
       }).catch(function(error){
         console.log('有错')
         console.log(error)
@@ -224,6 +331,39 @@ Page({
 
 
   },
+  getRoomList: function() {
+    const that = this
+
+    wx.request({
+      url: that.data.apiBaseUrl + '/room?accessToken=' + (wx.getStorageSync('token') || ''),
+      success: function (res) {
+        if (res.data) {
+          console.log(res.data)
+        }
+        // if (res.data && res.data.success) {
+        //   //获得 请求接口需要用到的token
+        //   wx.setStorageSync('token', res.data.token)
+        //   //获得 用户ID
+        //   that.setData({
+        //     userId: res.data.userid,
+        //     motto: that.data.motto + '(' + res.data.userid + ')'
+        //   })
+
+        //   that.getRoom()
+        // }
+      }
+    })
+
+  },
+  drawRoomList: function() {
+    const ctx = wx.createCanvasContext('roomListCanvas')
+    const p = this.data.canvasParam
+    ctx.setFillStyle("#ffffff");
+    ctx.rect(p.topLeftPad,5,p.topWidth,15)
+    ctx.fill()
+    ctx.draw(true);
+  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -287,27 +427,5 @@ Page({
   //
   // },
 
-  getRoom: function() {
-    console.log(this)
-    wx.request({
-      url: app.globalData.apiBaseUrl + '/room?accessToken=' + (wx.getStorageSync('token') || ''),
-      success: function (res) {
-        if (res.data) {
-          console.log(res.data)
-        }
-        // if (res.data && res.data.success) {
-        //   //获得 请求接口需要用到的token
-        //   wx.setStorageSync('token', res.data.token)
-        //   //获得 用户ID
-        //   that.setData({
-        //     userId: res.data.userid,
-        //     motto: that.data.motto + '(' + res.data.userid + ')'
-        //   })
 
-        //   that.getRoom()
-        // }
-      }
-    })
-
-  }
 })
